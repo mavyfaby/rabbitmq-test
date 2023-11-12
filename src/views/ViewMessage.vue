b6f72f03b2cb84d5cf548c45d5cc7207ae92b912
mavyfaby <maverickfabroa@gmail.com>
2023-11-12 22:31:32 +0800

    feat(app): reconnect if connection lost - add comments

--- src/views/ViewMessage.vue
+++ src/views/ViewMessage.vue
@@ -19,7 +19,9 @@
 
       <div class="flex items-center py-5">
         <md-filled-text-field placeholder="Send a message" v-model.trim="message" @keydown.enter="send" class="w-full" />
-        <md-filled-button class="ml-2" @click="send" :disabled="message.length === 0">Send</md-filled-button>
+        <md-filled-button class="ml-2" @click="send" :disabled="message.length === 0">
+          {{ isConnected ? "Send" : "Reconnect" }}
+        </md-filled-button>
       </div>
     </div>
   </div>
@@ -38,6 +40,7 @@ import ComposableMessage from "@composables/ComposableMessage.vue";
 
 const route = useRoute();
 const isLoading = ref(true);
+const isConnected = ref(false);
 const messages = ref([] as ChatMessage[]);
 const message = ref("");
 let username = "";
@@ -45,20 +48,25 @@ let username = "";
 let client: Paho.Client;
 
 onMounted(() => {
-  const { topic } = route.params;
+  // Get username from localstorage
   username = localStorage.getItem("username") || "anonymous";
-
+  // Initialize client
   client = new Paho.Client("localhost", 15675, "/ws", username);
 
+  // Set callbacks for connection lost
   client.onConnectionLost = (error: Paho.MQTTError) => {
     console.log("Connection lost", error);
+    isConnected.value = false;
   }; 
 
+  // Set callbacks for message arrived
   client.onMessageArrived = (message: Paho.Message) => {
     console.log("Message arrived", message);
 
-    const [topic, user] = message.destinationName.split("/");
+    // Get username from clientId
+    const [_, user] = message.destinationName.split("/");
 
+    // If the message is not from me, add it to the messages array
     if (user !== username) {
       messages.value.push({
         id: message.destinationName,
@@ -69,17 +77,30 @@ onMounted(() => {
     }
   };
 
-  client.onMessageDelivered = (message: Paho.Message) => {
+  // Set callbacks for message delivered
+  client.onMessageDelivered = (_: Paho.Message) => {
     console.log("Message Sent!");
   };
 
+  // Connect to the topic
+  connect(route.params.topic as string);
+});
+
+/**
+ * Connects to a topic nameserver 
+ * @param topic name of the topic to connect to
+ */
+function connect(topic: string) {
   console.info("Connecting...");
 
+  // Connect to RabbitMQ Web MQTT server
   client.connect({
     onSuccess: () => {
       console.log("Connected");
       isLoading.value = false;
+      isConnected.value = true;
 
+      // Subscribe to the topic with the username as the clientId (topic/username)
       client.subscribe(`${topic}/#`, {
         onSuccess: () => {
           console.log(`Subscribed to ${topic}!`);
@@ -88,23 +109,35 @@ onMounted(() => {
           console.log("Subscription failed", error);
         },
       });
-      
     },
     onFailure: (error: Paho.MQTTError) => {
       console.log("Connection failed", error);
       isLoading.value = false;
     },
   });
+}
 
-});
-
+/**
+ * Sends a message to the topic
+ */
 function send() {
-  if (!client) {
-    alert("Not connected");
+  // If the client is not initialized or not connected
+  if (!client || !client.isConnected()) {
+    // connect to the topic
+    if (client) {
+      connect(route.params.topic as string);
+      return;
+    }
+
+    // If the client is not initialized, alert the user
+    alert("Client not initialized!");
+    return;
   }
 
+  // Send the message to the topic
   client.send(`${route.params.topic}/${username}`, message.value);
 
+  // Add the message to the messages array
   messages.value.push({
     id: "9",
     username: "me",
@@ -116,6 +149,12 @@ function send() {
 }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+md-filled-text-field {
+  --md-filled-text-field-container-shape: 9999px;
+  --md-filled-field-active-indicator-height: 0px;
+  --md-filled-field-hover-active-indicator-height: 0px;
+  --md-filled-field-focus-active-indicator-height: 0px;
+}
 </style>
+
