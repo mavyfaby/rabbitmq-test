6864af8f17ee8ec4d0cc7e5b11394a10a1c03e0b
mavyfaby <maverickfabroa@gmail.com>
2023-11-12 08:51:49 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# RabbitMQ Test
+
+Utilizing RabbitMQ for Message Queuing Chat System with Material Design 3 UI
---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="container mx-auto">
+    
+  </div>
+</template>
+
+<style scoped>
+
+</style>
